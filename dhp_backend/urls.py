a3c637ed613f56b43aff725ea37b638540f58d53
amuhirwa <a.muhirwa@alustudent.com>
2025-07-11 04:46:44 +0200

    updated urls to all be similar

--- dhp_backend/urls.py
+++ dhp_backend/urls.py
@@ -9,11 +9,11 @@ from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('authentication.urls')),
     path('api/market/', include('marketplace.urls')),
     
     # Documentation URLs
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
