3577aad7e4a6a9a6c1c278c8d1ebcbc2b3c5fa03
amuhirwa <a.muhirwa@alustudent.com>
2025-07-18 19:58:02 +0200

    increase token lifetime

--- dhp_backend/settings.py
+++ dhp_backend/settings.py
@@ -134,8 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=60),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 }
