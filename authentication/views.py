f5412d9c0ea2dc0fefb3326031baed0346050ece
amuhirwa <a.muhirwa@alustudent.com>
2025-08-05 07:10:30 +0200

    add updating user info

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
+from turtle import up
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
 from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
 
@@ -13,4 +15,19 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == 'create':
             return [permissions.AllowAny()]
+        elif self.action in ['update', 'partial_update']:
+            return [permissions.IsAuthenticated()]
         return super().get_permissions()
+        
+    @action(detail=False, methods=['get', 'put', 'patch'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        if request.method == 'GET':
+            user = request.user
+            serializer = self.get_serializer(user)
+            return Response(serializer.data)
+        else:
+            user = request.user
+            serializer = self.get_serializer(user, data=request.data, partial=(request.method == 'PATCH'))
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
