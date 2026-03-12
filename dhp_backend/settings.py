3b46b8e6d721ed31d257f64f4dd32e22d539945f
Peace-Ishimwe <peaceishimwem@gmail.com>
2026-03-12 10:52:32 +0200

    cors

--- dhp_backend/settings.py
+++ dhp_backend/settings.py
@@ -146,6 +146,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://irage.rw",
+    "https://www.irage.rw",
     "https://dhp-platform.netlify.app",
     "http://127.0.0.1:5173",
 ]
